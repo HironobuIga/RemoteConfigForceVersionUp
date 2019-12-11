5a3bc2630affed16a2b1a1914fd5c9c164aa06fa
hironobu.iga <hironobu.iga.developer@gmail.com>
2019-12-12 00:56:39 +0900

    [fix]Firebaseの初期化前にRemoteConfigを使用していたため修正

--- RemoteConfigForceUpdate/RemoteConfigForceUpdate/ViewControllers/ViewController.swift
+++ RemoteConfigForceUpdate/RemoteConfigForceUpdate/ViewControllers/ViewController.swift
@@ -13,12 +13,17 @@ class ViewController: UIViewController {
     // MARK: - IBOutlet
     
     // MARK: - Property
+    private var remoteConfigPropertyProvider: RemoteConfigPropertyProvider!
 
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        updateViews()
+        showForceAlertIfNeeded()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: - Function
@@ -27,10 +32,41 @@ class ViewController: UIViewController {
 // MARK: - Private Function
 private extension ViewController {
     func setup() {
-        
+        remoteConfigPropertyProvider = RemoteConfigService.shared
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
     
-    func updateViews() {
+    @objc func applicationDidBecomeActive() {
+        showForceAlertIfNeeded()
+    }
+    
+    func showForceAlertIfNeeded() {
+        guard let forceAlertInformation = remoteConfigPropertyProvider.getForceAlertInformation() else { return }
         
+        let currentVersion = AppUtility.currentVersion
+        let criteriaVersion = forceAlertInformation.version
+        if VersionUtility().isForceAlertRequired(
+            currentVersion: currentVersion, criteriaVersion: criteriaVersion) {
+            showForceAlert(information: forceAlertInformation)
+        }
+    }
+    
+    func showForceAlert(information: ForceAlertInformation) {
+        let alertController = UIAlertController(
+            title: information.title,
+            message: information.message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            // okを押下後にAlertが非表示にならない用再度表示します
+            self?.showForceAlert(information: information)
+            if let url = information.url,
+                UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+            
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
